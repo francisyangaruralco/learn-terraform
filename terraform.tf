8dc1b96c05a1d54313eceeee6f4a7b8acdd28ced
ATS1\Francis.Yanga <francis.yanga@ruralco.co.nz>
2024-12-10 12:36:28 +1300

    Remove cloud block

--- terraform.tf
+++ terraform.tf
@@ -3,6 +3,7 @@
 
 terraform {
 
+/*
   cloud {
     organization = "Ruralco"
 
@@ -10,7 +11,7 @@ terraform {
       name = "learn-terraform"
     }
   }
-
+*/
   required_providers {
     aws = {
       source  = "hashicorp/aws"
